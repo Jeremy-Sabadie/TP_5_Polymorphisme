f5bc4ff47094370f92f8cf6f6368a30db81bd06f
cda07saba <jeremy.sabadie@2isa.org>
2023-01-20 09:43:49 +0100

    Ajoutez des fichiers projet.

---
+++ TP_5_Polymorphisme/Garagist.cs
@@ -0,0 +1,9 @@
+﻿namespace TP_5_Polymorphisme
+{
+    internal class Garagist
+    {
+        private string Name;
+        private string FirstName;
+        private double Skills;
+    }
+}
---
+++ TP_5_Polymorphisme/Véhicule.cs
@@ -0,0 +1,9 @@
+﻿namespace TP_5_Polymorphisme
+{
+    internal class Véhicule
+    {
+        private double Dommage;
+        private float TimeReparation;
+        private string Garagist;
+    }
+}
